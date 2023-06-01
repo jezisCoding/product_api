4da96fee66a4a68fdcef58565aad39fca930e5be
Samuel Dubcak <deagllle@gmail.com>
2023-06-01 17:08:40 +0200

    my first test!

--- api/data_api/tests.py
+++ api/data_api/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from copy import deepcopy
 
 # Create your tests here.
+
+class TestModelNameIdView(APITestCase):
+
+    def test_one_AttributeName_import(self):
+        ind = {"AttributeName": {"id": 1, "nazev": "Barva"}}
+        response = self.client.post(reverse("data_import"),
+                ind, format="json")
+ 
+        exd = deepcopy(ind)
+        exd["AttributeName"].update({"kod": "", "zobrazit": True})
+        exd = [exd]
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, exd)
--- api/data_api/urls.py
+++ api/data_api/urls.py
@@ -7,15 +7,17 @@ from .views import (
         )
 
 urlpatterns = [
-        path('import', ImportView.as_view()),
+        path('import', ImportView.as_view(), name = 'data_import'),
         path('import/', ImportView.as_view()),
 
-        path('detail/<str:model_name>', ModelNameListView.as_view()),
+        path('detail/<str:model_name>', ModelNameListView.as_view(), 
+            name = 'detail_model_list'),
         path('detail/<str:model_name>/', ModelNameListView.as_view()),
 
-        path('detail/<str:model_name>/<int:id>', ModelNameIdView.as_view()),
+        path('detail/<str:model_name>/<int:id>', ModelNameIdView.as_view(), 
+            name = 'detail_model_id'),
         path('detail/<str:model_name>/<int:id>/', ModelNameIdView.as_view()),
 
-        path('deletealldata', DeleteView.as_view()),
+        path('deletealldata', DeleteView.as_view(), name='delete_all'),
         path('deletealldata/', DeleteView.as_view()),
         ]
